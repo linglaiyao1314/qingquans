4e41c22e0cb1c594c2e972c051878735914fd7b6
linglaiyao1314 <xxxx@qq.com>
2016-03-19 23:25:40 +0800

    update

--- qsapp/__init__.py
+++ qsapp/__init__.py
@@ -1,5 +1,6 @@
 # coding=utf-8
-from flask import Flask, render_template, g, request, session
+from flask import Flask
+
 from helpers import template_filters
 
 
--- run_server.py
+++ run_server.py
@@ -3,4 +3,4 @@ from qsapp import create_app_by_config
 
 if __name__ == '__main__':
 	myapp = create_app_by_config()
-	myapp.run("0.0.0.0", port=80)
+	myapp.run("0.0.0.0", port=80, debug=True)
