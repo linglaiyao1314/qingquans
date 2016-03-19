300f9b14593e3ff8a1122ce9135765ad23185b99
linglaiyao1314 <xxxx@qq.com>
2016-03-19 21:16:40 +0800

    update

--- run_server.py
+++ run_server.py
@@ -4,4 +4,4 @@ from qsapp import create_app_by_config
 if __name__ == '__main__':
 	myapp = create_app_by_config()
 	print myapp.url_map
-	myapp.run("0.0.0.0", debug=True)
+	myapp.run("0.0.0.0", port=80)
